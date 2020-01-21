59153c96a67250aed7c8b72a69f6d7818c97404c
93jpark <93jpark@gmail.com>
2020-01-21 13:39:08 -0500

    test

--- JewelryBox.py
+++ JewelryBox.py
@@ -1,6 +1,7 @@
 #problem ID: jewelrybox
 
 import sympy as sym
+import math
 
 h = sym.Symbol('h')
 
@@ -11,8 +12,20 @@ x, y = input().split()
 x = int(x)
 y = int(y)
 
+a = 12
+b = -4 * (x+y)
+c = x*y
+d = b**2 + (-4 * a * c)
 
-
+r1 = (-b + math.sqrt(d)) / (2*a)
+r2 = (-b - math.sqrt(d)) / (2*a)
+if(r1 > r2):
+    r = r1
+elif(r1 < r2):
+    r = r2
+print(r)
+v = (x- (2*r)) * (y- (2*r)) * r
+print(v)
 #
 
 #list.append([x, y])
