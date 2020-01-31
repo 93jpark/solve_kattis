ac16a9f688480134118af291a1a1b7908c08e49a
Joonwoo Park <93jpark@gmail.com>
2020-01-30 21:14:18 -0500

    no message

--- Skener.py
+++ Skener.py
@@ -5,6 +5,7 @@ import numpy as np
 row, col, zr, zc = [int(x) for x in input().split()]
 
 list = []
+result = []
 for x in range(0,row):
     input_row = input()
     temp = []
@@ -15,5 +16,21 @@ for x in range(0,row):
 
 arr = np.array(list)
 print(arr)
+print()
+print(arr[:1,])
 
-#for x in range(row):
+
+"""
+
+for x in range(0, row):
+    temp_str = "";
+    for r in range(0, zr):
+        for y in range(0, col):
+            for c in range(0, zc):
+                temp_str += arr[x, y]
+    result.append(temp_str)
+    print(temp_str)
+else:
+    print(result)
+
+"""
